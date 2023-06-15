f882aba4530c4d3c5d6ce025a9ea3e9f1da58612
ahnge <ahnge226@gmail.com>
2023-06-15 20:22:50 +0630

    A bid of shading
    - ambient light & directional light
    - auto shader

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import WindowProperties
 from direct.actor.Actor import Actor
+
+from panda3d.core import WindowProperties, AmbientLight, Vec4, DirectionalLight
 
 
 class Game(ShowBase):
@@ -15,7 +16,7 @@ class Game(ShowBase):
 
         # Load the environment model
         self.environment = loader.loadModel("Models/Misc/Environment/environment")
-        # Attach it to Parent NodePath - global render variable
+        # Attach it to Parent NodePath - global render NodePath
         self.environment.reparentTo(render)
 
         # Load the actor and attach
@@ -27,6 +28,28 @@ class Game(ShowBase):
         self.tempActor.setPos(0, 0, 0)
         self.tempActor.getChild(0).setH(90)
         self.tempActor.loop("walk")
+
+        # Lighting
+
+        # AmbientLight returs Node
+        ambientLight = AmbientLight("ambient light")
+        ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+
+        # This time we take the parent NodePath and attach the node to it.
+        # `parent.attachNewNode(node)` returns the NodePath of the node
+        self.ambientLightNodePath = render.attachNewNode(ambientLight)
+
+        # Indicate which nodes we want to affect. In this case `render` NodePath
+        render.setLight(self.ambientLightNodePath)
+
+        # This is direction light
+        mainLight = DirectionalLight("main light")
+        self.mainLightNodePath = render.attachNewNode(mainLight)
+        # Turn it around by 45 degrees, and tilt it down by 45 degrees
+        self.mainLightNodePath.setHpr(45, -45, 0)
+        render.setLight(self.mainLightNodePath)
+
+        render.setShaderAuto()
 
         # Move the camera to a position high above the screen
         # --that is, offset it along the z-axis.
